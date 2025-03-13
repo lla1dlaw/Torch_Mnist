b669b6cd8bca71d832a568a63c60764abfef3689
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-03-12 22:44:41 -0400

    Altered network structure and prompted to save it

---
+++ main.py
@@ -0,0 +1,105 @@
+#Feed Forward Neural Network Trained and Tested on MNIST dataset
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+# devicce config
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# hyperparameters
+input_size = 784  # 28x28
+num_classes = 10
+hidden_size = 10
+num_hidden = 4
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+examples = iter(train_loader)
+samples, labels = next(examples)
+print(samples.shape, labels.shape)
+
+for i in range(6):
+    plt.subplot(2, 3, i + 1)
+    plt.imshow(samples[i][0], cmap='gray')
+
+
+# plt.show()
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_hidden, num_classes):
+        super(NeuralNet, self).__init__()
+        self.input_size = input_size
+        self.layers = [nn.Linear(input_size, hidden_size)]
+        for i in range(num_hidden-2): 
+            self.layers.append(nn.Linear(hidden_size, hidden_size))
+        self.layers.append(nn.Linear(hidden_size, num_classes))
+
+    def forward(self, x):
+        for layer in self.layers[:-1]:
+            x = layer(x)
+            x = nn.ReLU(x)
+        x = self.layers[-1](x)
+        # no activation and no softmax at the end
+        return x
+
+
+model = NeuralNet(input_size, hidden_size, num_hidden, num_classes).to(device)
+
+#Loss and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+#training loop
+n_total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # (100, 1, 28, 28)
+        # (100, 784)
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+
+        #forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        #backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if(i+1)% 100  == 0:
+            print(f'epoch {epoch+1}, step {i+1}/{n_total_step}, loss= {loss.item():.4f}')
+
+# Test the model
+# In test phase, we don't need to compute gradients (for memory efficiency)
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        # max returns (value ,index)
+        _, predicted = torch.max(outputs.data, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network on the 10000 test images: {acc} %')
+
+if input("Would you like to save this model? (y/n): ").lower() == "y":
+    model.save(model, 'model.pth')
+
+if input("See state dictionary of the model? (y/n): ").lower() == "y":
+    pprint(model.state_dict())
